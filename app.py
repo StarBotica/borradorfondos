60d8edfaad3dcaf7db2b8e2f295bd394b7b5d54d
citaconrama <48551285+StarBotica@users.noreply.github.com>
2023-04-22 21:46:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import streamlit as st
 from PIL import Image
 from PIL.ImageFilter import *
-#from rembg import remove
+# documentación: https://pillow.readthedocs.io/en/stable/reference/ImageFilter.html
 
 st.title("Eliminador de fondos")
 
-# subo fichero (por defecto no admite varios a la vez)
+filtros = ['BLUR','CONTOUR','DETAIL']
+with st.sidebar:
+  seleccionados = st.multiselect("Elige los filtros que deseas aplicar...",filtros)
+
+# subo fichero
+# (por defecto no admite varios a la vez)
+# especificando el tipo de fichero para que sea una imagen
 fichero = st.file_uploader("Elige tu imagen...",type=['png', 'jpg'])
 
-if fichero:
-  
+if fichero:  
   imagen_subida = Image.open(fichero)
+  # mostramos la imagen original
   st.image(imagen_subida)
-  #fixed = remove()
-  imagen_editada = imagen_subida.filter(BLUR)
+  # aplicamos los filtros seleccionados
+  if 'BLUR' in seleccionados:
+    imagen_editada = imagen_subida.filter(BLUR)
+  if 'CONTOUR' in seleccionados:
+    imagen_editada = imagen_subida.filter(CONTOUR)
+  if 'DETAIL' in seleccionados:
+    imagen_editada = imagen_subida.filter(DETAIL)
   st.image(imagen_editada)
   st.download_button("Descarga tu imagen aquí",fichero,mime="image/png")
