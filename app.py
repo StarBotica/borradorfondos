d88f74909c1d0b9d1494caa2f8fe919cab6ec630
citaconrama <48551285+StarBotica@users.noreply.github.com>
2023-04-22 20:29:14 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Eliminador de fondos")
+
+# subo fichero (por defecto no admite varios a la vez)
+fichero = st.file_uploader("Elige tu imagen...",type=['png', 'jpg'])
+
+if fichero:
+  st.download_button(fichero)
